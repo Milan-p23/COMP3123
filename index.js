7fb33e114a38ba45e842696faddbb378f52ccfcc
Milan Patel <milanapatel23@gmail.com>
2024-09-06 19:38:40 -0400

    js code init

---
+++ index.js
@@ -0,0 +1,14 @@
+console.log('Hello, world!'); 
+
+//object literals
+
+var students = {
+    name: 'John',
+    age: 20,
+    grades: [90, 85, 95],
+    city: 'New York'
+}
+
+console.log(students);
+console.log(students.name);
+console.log(typeof students)
