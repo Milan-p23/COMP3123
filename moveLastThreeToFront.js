080df2f56d0bb554a4de9f7e6d43438ef306705e
Milan Patel <milanapatel23@gmail.com>
2024-09-07 22:36:13 -0400

    Lab Exercise 1

---
+++ moveLastThreeToFront.js
@@ -0,0 +1,10 @@
+function right(str) {
+    if (str.length >= 3) {
+        return str.slice(-3) + str.slice(0, -3);
+    }
+    return str;
+}
+
+console.log(right('Python'));     
+console.log(right('JavaScript')); 
+console.log(right('Hi'));       
